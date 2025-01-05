6cbbb7c0d4d65638b5ccd5d83b5221ecb103cc4a
Louis Drotos <louis.drotos@gmail.com>
2025-01-04 21:24:31 -0500

    Updated certificate script to create and sign server certificate

--- scripts/create_certs.sh
+++ scripts/create_certs.sh
@@ -12,5 +12,21 @@ winpty openssl req -x509 -sha256 -days 3650 -newkey rsa:4096 \
   -subj "//SKIP=skip/CN=fairfaxfire.gov/emailAddress=admin@fairfaxfire.gov/C=US/ST=Virginia/L=Fairfax/O=Fairfax County/OU=Fire and Rescue" \
   -passin pass:'password' \
   -passout pass:'password'
-
 echo "Root certificate created"
+
+echo "Creating server certicate CSR"
+winpty openssl req -new -newkey rsa:4096 \
+  -keyout "${SCRIPT_DIR}/certs/ffx-fire-server.key" \
+  -out "${SCRIPT_DIR}/certs/ffx-fire-server.csr" \
+  -subj "//SKIP=skip/CN=localhost/emailAddress=admin@fairfaxfire.gov/C=US/ST=Virginia/L=Fairfax/O=Fairfax County/OU=Fire and Rescue" \
+  -passin pass:'password' \
+  -passout pass:'password'
+echo "Server certificate created"
+
+echo "Signing server certificate"
+winpty openssl x509 -req -CA "${SCRIPT_DIR}/certs/ffx-fire-root-CA.crt" -CAkey "${SCRIPT_DIR}/certs/ffx-fire-root-CA.key" \
+  -in "${SCRIPT_DIR}/certs/ffx-fire-server.csr" \
+  -out "${SCRIPT_DIR}/certs/ffx-fire-server.crt" \
+  -passin pass:'password' \
+  -days 365 -CAcreateserial -extfile "${SCRIPT_DIR}/localhost.ext"
+echo "Server certificate signed"
